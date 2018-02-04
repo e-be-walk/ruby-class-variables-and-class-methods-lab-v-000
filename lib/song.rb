4f70543b0b063dfe0a4cf43fdc6524c87569f0c3
Erin Walker <erinb0317@hotmail.com>
2018-02-04 01:40:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,20 @@
 class Song
 
-  attr_accessor :name, :artist, :genre
+  attr_accessor :name, :artist, :genres
 
   @@count = 0
+  @@genres = []
 
   def initialize
     @@count += 1
+    @@genres >> genres
   end
 
   def self.count
     @@count
   end
+
+  def genres(genre)
+    genre.each do |genre|
+      genre.uniq! { |s| s.first }
 end
